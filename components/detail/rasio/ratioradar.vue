8f2a9d56b5f5be4d43e02eb20898739878119427
maziyank <maziyank@gmail.com>
2021-01-12 18:15:39 +0700

    fix radar color

--- components/detail/rasio/ratioradar.vue
+++ components/detail/rasio/ratioradar.vue
@@ -17,8 +17,8 @@ export default {
           {
             label: 'Rasio',
             fill: true,
-            backgroundColor: '#b3af8f',
-            borderColor: '#ffc482',
+            backgroundColor: 'rgb(127 41 130 / 40%)',
+            borderColor: '#7f2982',
             pointBorderColor: '#fff',
             pointBackgroundColor: 'rgba(179,181,198,1)',
             data: [0.77, 0.61, 0.69, 0.62, 0.82, 0.6]
@@ -39,7 +39,7 @@ export default {
   methods: {
     refreshData () {
       this.chartData.labels = this.ratios.filter(x => x.value <= 1).map(x => x.title)
-      this.chartData.datasets[0].data = this.ratios.filter(x => x.value <= 1).map(x => x.value)
+      this.chartData.datasets[0].data = this.ratios.filter(x => x.value <= 1).map(x => x.value.toFixed(2))
     }
   }
 }
