93a39cbd821705f4cb397e3a6ef528d430aa988f
maziyank <bakhtiar.amaludin@kemenkeu.go.id>
2021-01-13 11:31:27 +0700

    fix year sync

--- pages/detail/_id/index.vue
+++ pages/detail/_id/index.vue
@@ -145,7 +145,8 @@ export default {
         this.year = evaluateXPathToString(
           "//general/info[@name='Tahun Anggaran']/text()",
           this.xbrlDoc
-        );
+        ).trim();
+        console.log(this.year);
       }, 1000);
     },
     downloadUrl(url) {
